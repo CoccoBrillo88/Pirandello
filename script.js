a33f47428ca050b652fa32e4172c4c34ee92622a
CoccoBrillo88 <248153581+CoccoBrillo88@users.noreply.github.com>
2026-04-17 18:37:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-window.addEventListener("load", () => {
-    const cards = document.getElementById("cards");
+// Animazione semplice in entrata
 
-    setTimeout(() => {
-        cards.classList.remove("hidden");
-        cards.classList.add("show");
-    }, 800);
+const elements = document.querySelectorAll(".grid, .card-section");
+
+window.addEventListener("scroll", () => {
+    elements.forEach(el => {
+        const position = el.getBoundingClientRect().top;
+        const screen = window.innerHeight;
+
+        if (position < screen - 100) {
+            el.style.opacity = 1;
+            el.style.transform = "translateY(0)";
+        }
+    });
+});
+
+// stato iniziale
+elements.forEach(el => {
+    el.style.opacity = 0;
+    el.style.transform = "translateY(40px)";
+    el.style.transition = "all 0.6s ease";
 });
